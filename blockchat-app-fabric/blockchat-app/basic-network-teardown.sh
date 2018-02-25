5ef4d97722b52f2189765459b5394a21cad3dc8e
user <user@blockchatvm>
2018-02-25 16:54:02 -0500

    updated scripts to restart the network and the server

--- blockchat-app-fabric/blockchat-app/basic-network-teardown.sh
+++ blockchat-app-fabric/blockchat-app/basic-network-teardown.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-(cd ../basic-network; ./teardown.sh)
+
+(cd ../basic-network; ./stop.sh; ./teardown.sh)
+
+echo
+echo "FORCING the clean up of all images ..."
+docker rm $(docker stop $(docker ps -aq))
+docker rmi -f $(docker images | grep "dev"- | cut -f 1 -d ' ' -)
+rm -rf ~/.hfc-key-store /tmp/hfc/
+
